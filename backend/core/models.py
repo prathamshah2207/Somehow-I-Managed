bbfd28c9291c8b014d92cc074054090f038285c8
mvr659 <mvr659@usask.ca>
2025-11-05 22:50:28 +0530

    Updated UserProfile model to have a display name and age and can be represented as a string

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+	# user_name = models.OneToOneField(User, on_delete=models.CASCADE)
+	display_name = models.CharField(max_length=100)
+	age = models.PositiveIntegerField()
+	
+	def __str__(self):
+		return self.display_name
