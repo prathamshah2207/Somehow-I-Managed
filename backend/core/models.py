30e3f08f450a7aa5bed43ddf8eecb02682e9bdf9
mvr659 <mvr659@usask.ca>
2025-11-06 00:27:34 +0530

    created signup api endpoint in the views.py in core

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
 	user_credentials = models.OneToOneField(User, on_delete=models.CASCADE)
-	user_email = models.EmailField()
 	display_name = models.CharField(max_length=100)
 	age = models.PositiveIntegerField(null=True, blank=True)
 	
