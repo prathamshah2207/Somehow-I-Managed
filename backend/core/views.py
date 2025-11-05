19959f8508fa884a188d4f64e61d3c0a12fa7472
mvr659 <mvr659@usask.ca>
2025-11-06 01:07:25 +0530

    bug fixes in the api endpoint for signup and added the url in the appropriate file and configured it.

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from models import UserProfile
+from .models import UserProfile
 
 def index(request):
     return HttpResponse("Hello, world. We are at the core's index.")
