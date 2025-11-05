32628dac7921a732b09a8e1e93f7db7cad323f49
mvr659 <mvr659@usask.ca>
2025-11-06 00:06:53 +0530

    Added new email variable for user in the UserProfile model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-	# user_name = models.OneToOneField(User, on_delete=models.CASCADE)
+	user_credentials = models.OneToOneField(User, on_delete=models.CASCADE)
+	user_email = models.EmailField()
 	display_name = models.CharField(max_length=100)
-	age = models.PositiveIntegerField()
+	age = models.PositiveIntegerField(null=True, blank=True)
 	
 	def __str__(self):
 		return self.display_name
