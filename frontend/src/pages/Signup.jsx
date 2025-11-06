94df50e0cdf367c2f9fe1d1daa3b42b7a87fa3af
Hitarth Bhatia <somehowimanaged2510@gmail.com>
2025-11-05 21:25:47 -0600

    Changed the position of email, passwird, username, display name

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -7,12 +7,14 @@ function Signup() {
       <div className="glass-card">
         <h2 className="title">Create Account</h2>
         <form className="signup-form">
-          <input type="text" placeholder="Display Name" required />
-          <input type="email" placeholder="Email" required />
-          <input type="text" placeholder="Username" required />
-          <input type="password" placeholder="Password" required />
-          <button type="submit">Sign Up</button>
-        </form>
+  <input type="email" placeholder="Email" required />
+  <input type="text" placeholder="Username" required />
+  <input type="password" placeholder="Password" required />
+  <input type="text" placeholder="Display Name" required />
+  <button type="submit">Sign Up</button>
+</form>
+
+        
       </div>
     </div>
   );
