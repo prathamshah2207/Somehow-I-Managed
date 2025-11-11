310718f018228b08f0224c93e290b1f7530eb63c
mvr659 <mvr659@usask.ca>
2025-11-12 01:11:28 +0530

    Added whitenoice to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -91,5 +92,7 @@ USE_TZ = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
